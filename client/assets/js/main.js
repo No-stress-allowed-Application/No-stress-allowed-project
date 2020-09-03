5be4db1cbd31d2d57b3cb4265bef976cd5961691
abdulfattah94 <abdulfattah.1994@gmail.com>
2020-09-04 01:40:46 +0700

    ajax query login dan regist

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -6,7 +6,69 @@ beforeLogin = () => {
     $('header').hide()
     $('#login').show()
 }
-
+afterLogin = () => {
+    $('header').show()
+    $('#register').hide()
+    $('#login').hide()   
+}
+register = e => {
+    e.preventDefault()
+    const username = $('#username-register').val()
+    const email = $('#email-register').val()
+    const password = $('#password-register').val()
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/users/register',
+        data: {
+            username,
+            email,
+            password
+        }
+    })
+    .done(res => {
+        $('#username-register').val('')
+        $('#email-register').val('')
+        $('#password-register').val('')
+        beforeLogin()
+        $('.msg').append(`<div class="alert alert-success" role="alert">Account created successfully!</div>`)
+    })
+    .fail(err => {
+        for(const el of err.responseJSON.errors){
+            $('.msg').append(`<div class="alert alert-danger" role="alert">${el}</div>`)
+        }
+    })
+}
+login = e => {
+    e.preventDefault()
+    const email = $('#email-login').val()
+    const password = $('#password-login').val()
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/users/login',
+        data: {
+            email,
+            password
+        }
+    })
+    .done(res => {
+        $('#email-login').val('')
+        $('#password-login').val('')
+        localStorage.setItem('access_token', res.access_token)
+        afterLogin()
+        $('.msg').append(`<div class="alert alert-success" role="alert">Login success!</div>`)
+    })
+    .fail(err => {
+        for(const el of err.responseJSON.errors){
+            $('.msg').append(`<div class="alert alert-danger" role="alert">${el}</div>`)
+        }
+    })
+}
+logout = () => {
+    $('.msg').empty()
+    beforeLogin()
+    localStorage.clear()
+    $('.msg').append(`<div class="alert alert-success" role="alert">You've logged out!</div>`)
+}
 $(document).ready(() => {
     initContent()
 
@@ -16,8 +78,7 @@ $(document).ready(() => {
         beforeLogin()
     }  
 
-    $('button, a.btn, a.register-link, a.login-link').click(e => {
-        e.preventDefault()
+    $('button, a.btn, a.register-link, a.login-link').click( () => {       
         $('.msg').empty()
     })
 
@@ -30,6 +91,10 @@ $(document).ready(() => {
         $('#register').hide()
         $('#login').show()        
     })
+
+    $('#register-form').submit(register)
+    $('#login-form').submit(login)
+    $('#logout').click(logout)
 
     
 })
