a3ad68f0766aa4084cca401048a9b48027e04403
SamuelGirsang <samuel.sautdo.p.girsang@gmail.com>
2020-09-04 10:00:25 +0700

    pexels-api

---
+++ server/controllers/PexelController.js
@@ -0,0 +1,20 @@
+const axios = require('axios').default;
+
+class PexelController {
+  static getPexel (req, res, next) {
+    axios({
+      method:"GET",
+      url : "http://api.pexels.com/v1/search?query=example+query&per_page=15&page=1",
+      headers: {
+      'Authorization': '563492ad6f917000010000014eb73d02e9134f9d8f70fa9d5b0c39cc'
+      } 
+    })
+    .then((response)=>{
+      console.log(response.data)
+      res.status(200).json({res})
+    })
+    .catch(err => next(err))
+  }
+}
+
+module.exports = PexelController
